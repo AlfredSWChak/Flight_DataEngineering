cb4441c98cd8c147f81a92ef832ee3fec75c53e0
AlfredSWChak <alfredswac.nl@gmail.com>
2025-02-20 00:09:29 +0100

    Add a few functions in part3.py

--- mainFile.py
+++
@@ -1,3 +0,0 @@
-import part12 as pt12
-import part3 as pt3
-
--- part3.py
+++ part3.py
@@ -2,13 +2,38 @@ import sqlite3
 import pandas as pd
 
 connection = sqlite3.connect('flights_database.db')
-
-query = f'SELECT * FROM flights'
-
 cursor = connection.cursor()
-cursor.execute(query)
 
-rows = cursor.fetchall()
-whole = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
+# There are five tables: {airlines, airports, flights, planes, weather}
+def getTable(input):
+    
+    tableName = str(input)
+    query = f'SELECT * FROM [{tableName}]'
+    cursor.execute(query)
+    
+def getTable_Equal(table, column, faa):
+    
+    columnName = str(column)
+    tableName = str(table)
+    query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] = ?'
+    cursor.execute(query, (faa,))
+                   
+def getTable_Larger(table, column, value):
+    
+    columnName = str(column)
+    tableName = str(table)
+    query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] > ?'
+    cursor.execute(query, (value,))
+    
+def getTable_Smaller(table, column, value):
+    
+    columnName = str(column)
+    tableName = str(table)
+    query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] < ?'
+    cursor.execute(query, (value,))
 
-print (whole)
+def printTable():
+    rows = cursor.fetchall()
+    all = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
+    
+    print(all)
