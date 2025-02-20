f33fdfb297f76b777f880e891b183c9f1aa96e38
AlfredSWChak <alfredswac.nl@gmail.com>
2025-02-20 12:26:30 +0100

    Export tables from databases and add one new function in part3.py

--- core.py
+++ core.py
@@ -1,32 +1,17 @@
 import part1 as pt1
 import part3 as pt3
 
-
-# Just printing out the tables in database for review
-pt3.getTable("airlines")
-pt3.printTable()
-
-pt3.getTable("airports")
-pt3.printTable()
-
-pt3.getTable("flights")
-pt3.printTable()
-
-pt3.getTable("planes")
-pt3.printTable()
-
-pt3.getTable("weather")
-pt3.printTable()
-
 # functions that can be used to access the database in specific column
 pt3.getTable_Equal("airports","faa","JFK")
-pt3.printTable()
+# pt3.printTable()
 
 pt3.getTable_Larger("flights","distance",2000)
-pt3.printTable()
+# pt3.printTable()
 
 pt3.getTable_Larger("planes","seats",200)
-pt3.printTable()
+# pt3.printTable()
 
 pt3.getTable_Smaller("weather","visib",1.0)
-pt3.printTable()
+# pt3.printTable()
+
+pt3.printFlightsOnDateAtAirport(12, 17, "JFK")
--- part3.py
+++ part3.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import part1 as pt1
 
 connection = sqlite3.connect('flights_database.db')
 cursor = connection.cursor()
@@ -10,6 +11,7 @@ def getTable(input):
     tableName = str(input)
     query = f'SELECT * FROM [{tableName}]'
     cursor.execute(query)
+    return
     
 def getTable_Equal(table, column, faa):
     
@@ -17,6 +19,7 @@ def getTable_Equal(table, column, faa):
     tableName = str(table)
     query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] = ?'
     cursor.execute(query, (faa,))
+    return
                    
 def getTable_Larger(table, column, value):
     
@@ -24,6 +27,7 @@ def getTable_Larger(table, column, value):
     tableName = str(table)
     query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] > ?'
     cursor.execute(query, (value,))
+    return
     
 def getTable_Smaller(table, column, value):
     
@@ -31,9 +35,56 @@ def getTable_Smaller(table, column, value):
     tableName = str(table)
     query = f'SELECT * FROM [{tableName}] WHERE [{columnName}] < ?'
     cursor.execute(query, (value,))
+    return
 
 def printTable():
     rows = cursor.fetchall()
     all = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
     
     print(all)
+    return
+    
+def fetch_table_data(table_name):
+
+    cursor.execute('select * from ' + table_name)
+    header = [row[0] for row in cursor.description]
+    rows = cursor.fetchall()
+
+    return header, rows
+
+def export(table_name):
+    
+    header, rows = fetch_table_data(table_name)
+    
+    # Create csv file
+    file = open(table_name + '.csv', 'w')
+    # Write header
+    file.write(','.join(header) + '\n')
+    
+    for row in rows:
+        file.write(','.join(str(r) for r in row) + '\n')
+        
+    file.close()
+    print(str(len(rows)) + ' rows written successfully to ' + file.name)
+    return
+    
+def showAllTableNames():
+    query = f"SELECT name FROM sqlite_master WHERE type='table';"
+    cursor.execute(query)
+    
+    print(cursor.fetchall())
+    return
+    
+def printFlightsOnDateAtAirport(month, day, airport):
+    
+    query = f'SELECT month, day, origin, dest FROM flights WHERE month = ? AND day = ? AND origin = ?'
+    cursor.execute(query, [month, day, airport])
+    
+    rows = cursor.fetchall()
+    new_df = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
+    
+    faaList = new_df['dest']
+    
+    pt1.drawMultipleLines(faaList)
+    
+    return
