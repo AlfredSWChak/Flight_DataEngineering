a403ced6b2e04cd8f1fa84845c5d014406423f3b
AlfredSWChak <alfredswac.nl@gmail.com>
2025-03-03 14:43:15 +0100

    Add and update a few functions

--- part1.py
+++ part1.py
@@ -9,8 +9,21 @@ import math
 
 file = pd.read_csv("airports_original.csv")
         
-# fig = px.scatter_geo(file, hover_name="name", lat="lat", lon = "lon", color="alt")
 fig = px.scatter_geo()
+
+def showAllAirports():
+    
+    fig = px.scatter_geo(file, hover_name='name', 
+                         lat='lat', 
+                         lon = 'lon', 
+                         color='alt',
+                         color_continuous_scale='rainbow',
+                         size_max=1)
+    fig.update_layout(title = 'All the airports with different altitudes')
+    fig.update_coloraxes(colorbar_title_text='Altitude of airports')
+    fig.show()
+    
+    return
 
 def drawLine(inputFAA, fig):
 
@@ -55,7 +68,6 @@ def drawMultipleLines(faaList, month, day, origin_faa):
                 new_df.loc[j] = file.iloc[j]
     
     origin_row = getAirportRow(origin_faa).iloc[0]
-    # NYC_row = getJFK().iloc[0]
     origin_lon = origin_row["lon"]
     origin_lat = origin_row["lat"]
     new_df.loc[origin_row.index[0]] = origin_row
@@ -68,7 +80,7 @@ def drawMultipleLines(faaList, month, day, origin_faa):
         fig.add_trace(go.Scattergeo(locationmode = 'USA-states',lon = [input_lon, origin_lon], lat = [input_lat, origin_lat], mode = "lines", line = dict(width = 1,color = 'red'), opacity = 1))
     
     fig.update_layout(title_text = 'Flights to New York from specific locations', showlegend = False)
-    fig.update_layout(title = 'Map of Flight from ' + origin_faa + ' on ' + str(day) + '/' + str(month) ,geo_scope="usa")
+    fig.update_layout(title = 'Flights departed from ' + origin_faa + ' on ' + str(day) + '/' + str(month) ,geo_scope="usa")
     fig.show()
     return
 
@@ -110,12 +122,9 @@ def calculateDistances():
     plt.show()
     return distance_np
 
-# randomlists = ["ALX", "BKC", "BKG", "HOT", "LFI", "MLL", "PBF", "TUP"]
-# drawMultipleLines(randomlists)
-
-earthRadius = 6378
-
 def geodesicDistance():
+    
+    earthRadius = 6378
     
     NYC_row = getJFK().iloc[0]
     NYC_lon = NYC_row["lon"]
@@ -144,46 +153,12 @@ def analyzeTimeZone():
 
     tz_file = file.dropna(subset=['tz'])
 
-    fig = px.scatter_geo(tz_file, hover_name="name", lat="lat", lon="lon", color="tz")
+    fig = px.scatter_geo(tz_file, hover_name='name', 
+                         lat='lat', lon='lon', color='tz',
+                         color_continuous_scale='rdbu',)
+    
+    fig.update_layout(title = 'All the airports with different time zones')
+    fig.update_coloraxes(colorbar_title_text='Time zones')
     fig.show()
     
     return
-
-# def getCity():
-
-#     cityList = []
-    
-#     for i in range(len(file)):
-#         thisAirport = file.iloc[i]
-        
-#         if (pd.isnull(thisAirport['tzone'])):
-#             city = thisAirport['tzone']
-#         else:
-#             city = thisAirport['tzone'].split('/')[1]
-#             city = city.replace('_',' ')   
-             
-#         cityList.append(city)
-    
-#     newFile = file.assign(citys = cityList)
-#     newFile = newFile.dropna(subset=['citys'])
-    
-#     statesFile = pd.read_csv("states.csv")
-#     abbrevList = []
-    
-#     for i in range(len(newFile)):
-#         thisAirport = newFile.iloc[i]
-        
-#         for j in range(len(statesFile)):
-#             if (thisAirport['citys'] == statesFile.iloc[j]['State']):
-#                 abbrev = statesFile.iloc[j]['Abbreviation']
-#                 break
-        
-#         abbrevList.append(abbrev)
-    
-#     newFile = newFile.assign(abbrevs = abbrevList)
-    
-#     print(newFile)
-    
-#     return
-
-# getCity()
