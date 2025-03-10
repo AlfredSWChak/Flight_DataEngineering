29fe4b7ef2f3be3b98b22d1c9b51342f29191a8c
AlfredSWChak <alfredswac.nl@gmail.com>
2025-03-10 22:41:54 +0800

    Add two more new functions in dashboard

--- alfred_function.py
+++ alfred_function.py
@@ -29,5 +29,30 @@ def top_five_planes():
     
     return result
 
-
+def top_five_flights(airport):
+    
+    if(airport == 'All airports'):
+        query = f'SELECT origin, dest, distance FROM flights'
+        cursor.execute(query)
+    else:
+        query = f'SELECT origin, dest, distance FROM flights WHERE origin = ?'
+        cursor.execute(query, (airport,))
+    
+    rows = cursor.fetchall()
+    flights_df = pd.DataFrame(rows, columns = [x[0] for x in cursor.description])
+    
+    unique_flights_df = flights_df.drop_duplicates(subset=['origin', 'dest'])
+    
+    count_flights_df = flights_df.groupby(by=['origin', 'dest']).size().reset_index(name='numFlights')
+    top_five = count_flights_df.sort_values(by=['numFlights'], ascending=False).head()
+    
+    result = pd.DataFrame()
+    
+    for origin, dest, numFlights in zip(top_five['origin'], top_five['dest'], top_five['numFlights']):
+        result = pd.concat([result, unique_flights_df[(unique_flights_df['origin'] == origin) & (unique_flights_df['dest'] == dest)]])
+    
+    result = result.assign(numFlights=list(top_five['numFlights']))
+    result = result.astype({'distance':'int'})
+    
+    return result
 
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import alfred_function as af
 import part3 as pt3
+import calendar
+from datetime import datetime
 
 st.set_page_config(page_title = 'Project Flights', 
                    layout = 'wide', 
@@ -9,7 +11,11 @@ st.set_page_config(page_title = 'Project Flights',
 st.sidebar.title('Functions')
 home = st.sidebar.button('Home Page')
 
-options_set = ('Flight statistics on specific day', 'Top five plane models')
+options_set = ('Flight statistics on specific day', 
+               'Top five plane models', 
+               'Top five flights',
+               'Among of delay flights')
+month_list = list(calendar.month_name)[1:]
 
 add_selectbox = st.sidebar.radio('Options', 
                                  options=options_set, 
@@ -55,3 +61,40 @@ elif add_selectbox == 'Top five plane models':
     with c_1:
         result = af.top_five_planes()
         st.table(result.set_index(result.columns[0]))
+        
+elif add_selectbox == 'Top five flights':
+    st.header('Top 5 flights')
+    
+    c_1 = st.container()
+    
+    with c_1:
+        airport = st.selectbox('Select a airport',['EWR', 'LGA', 'JFK', 'All airports'])
+        button_clicked = st.button('Submit')
+        
+    c_2 = st.container(border=True)  
+        
+    with c_2:
+        if button_clicked:
+            result = af.top_five_flights(airport)
+            st.table(result.set_index(result.columns[0]))
+        
+elif add_selectbox == 'Among of delay flights':
+    st.header('Among of delay flights')
+    
+    input_start_month = st.selectbox('Select the month of start:',month_list)
+    input_end_month = st.selectbox('Select the end of month',month_list[month_list.index(input_start_month):])
+    
+    dest_list = sorted(pt3.unique_arrive_airports())
+    dest = st.selectbox('Select a destination airport',dest_list)
+    button_clicked = st.button('Submit')
+    
+    c_1 = st.container(border=True)
+    
+    with c_1:
+        if button_clicked:
+            
+            start_month = datetime.strptime(input_start_month, '%B').month
+            end_month = datetime.strptime(input_end_month, '%B').month
+            amount = pt3.amongOfDelayFlights(start_month, end_month, dest)
+            
+            st.write('For destination',dest,'during month',input_start_month,'to',input_end_month,', the amount of delay flights is',amount,'.')
